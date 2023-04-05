d52b0aac887ddbaeeeb75f4c7c5440e6b63cfa01
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2023-04-05 14:54:03 +0900

    feat: copied booknote client into different repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, this is kazuki trying to create booknote</h1>
+    </div>
+  );
+}
+
+export default App;
