a09656bcb01f0ae868afd289e18516aed8e84858
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2023-04-09 20:54:03 +0900

    chore: debugging deployment failure

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="login" element={<Login />} />
-          <Route path="home" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
