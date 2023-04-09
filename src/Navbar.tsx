9eaf0703591de800d936fc16c156304875ff8d14
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2023-04-09 20:59:00 +0900

    chore: debugging deployment failure

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,10 +6,10 @@ const Navbar = () => {
     <>
       <div className="Navbar">
         <h1>BookNotes</h1>
-        <img
+        {/* <img
           src="https://cdn.pixabay.com/animation/2022/12/05/05/08/05-08-32-681_512.gif"
           className="gif"
-        />
+        /> */}
       </div>
     </>
   );
